06a99a3ac8f5d481eab65310c0afa43f3a7068c9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-20 02:01:38 +0000

    Adding tarfile member sanitization to extractall()

--- tile.py
+++ tile.py
@@ -53,7 +53,29 @@ for h in range(8):
                         os.mkdir(img_path)
                         tarball = os.path.join(frames_data, "{}.tar".format(img_fn[:-len("_000000.jpg")]))
                         with tarfile.open(tarball, 'r') as tar:
-                            tar.extractall(path=img_path)
+                            
+                            import os
+                            
+                            def is_within_directory(directory, target):
+                                
+                                abs_directory = os.path.abspath(directory)
+                                abs_target = os.path.abspath(target)
+                            
+                                prefix = os.path.commonprefix([abs_directory, abs_target])
+                                
+                                return prefix == abs_directory
+                            
+                            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                            
+                                for member in tar.getmembers():
+                                    member_path = os.path.join(path, member.name)
+                                    if not is_within_directory(path, member_path):
+                                        raise Exception("Attempted Path Traversal in Tar File")
+                            
+                                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                                
+                            
+                            safe_extract(tar, path=img_path)
                         os.unlink(tarball)
                     img = Image.open(os.path.join(img_path, img_fn))
                     if img.size[0]/float(img.size[1]) > 16.0/9.0:
